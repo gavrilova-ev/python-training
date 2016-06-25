8b1ef9ec7ee960e2ea90c12ee45d44d9e2449ddc
Ekaterina Gavrilova <ekaterina.gavrilova@sap.com>
2016-06-25 18:47:42 +0200

    1.The same with a function call

--- Exercise4/mypolyglot.py
+++ Exercise4/mypolyglot.py
@@ -4,8 +4,11 @@ world = TurtleWorld()
 bob = Turtle()
 print bob
 
-for i in range(4):
-	fd(bob,100)
-	lt(bob)
+def square(t):
+	for i in range(4):
+		fd(t, 100)
+		lt(t)
+		
+square(bob)
 	
 wait_for_user()
