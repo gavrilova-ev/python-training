5e8f4cb55f7ccff3946ce4aacb72d2d3e8f1244f
Ekaterina Gavrilova <ekaterina.gavrilova@sap.com>
2016-06-25 18:50:53 +0200

    2.Add length as a parameter

--- Exercise4/mypolyglot.py
+++ Exercise4/mypolyglot.py
@@ -4,11 +4,16 @@ world = TurtleWorld()
 bob = Turtle()
 print bob
 
-def square(t):
+def square(t, length):
 	for i in range(4):
-		fd(t, 100)
+		fd(t, length)
 		lt(t)
 		
-square(bob)
+square(bob, 25)
+square(bob, 50)
+square(bob, 100)
+square(bob, 150)
+square(bob, 200)
+square(bob, 250)
 	
 wait_for_user()
